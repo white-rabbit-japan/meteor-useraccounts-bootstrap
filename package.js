214b0b597c6fcb6322c1dd5394a822738d6e6348
Max Hodges <max@whiterabbitjapan.com>
2015-03-22 23:53:13 +0900

    change package name
    forked

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Accounts Templates styled for Twitter Bootstrap.",
     version: "1.8.1",
-    name: "useraccounts:bootstrap",
+    name: "whiterabbit:meteor-useraccounts-bootstrap",
     git: "https://github.com/meteor-useraccounts/bootstrap.git",
 });
 
